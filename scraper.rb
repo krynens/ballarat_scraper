360a7306cb83ac90a8066b6e78a049af5f32f38d
Thom Krynen <75901402+krynens@users.noreply.github.com>
2021-03-08 15:49:31 +1100

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -19,7 +19,7 @@ capybara = Capybara::Session.new(:selenium_chrome_headless_morph)
 
 capybara.visit("https:/totaljoy.co/read/")
 
-titles = capybara.find_element('summary-title')
+titles = capybara.find_by_css('summary-title')
 
 print(titles)
 
